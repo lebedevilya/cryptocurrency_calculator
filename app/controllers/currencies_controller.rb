4da128f39a22882640f2ce21a66191b0f7b732c2
Ilya Lebedev <ilyalebedev1@yandex.ru>
2019-05-30 02:02:39 +0300

    react init commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,8 +3,27 @@ class CurrenciesController < ApplicationController
 	end
 
 	def search
+    @currencies = Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
+    render json: {currencies: @currencies}
 	end
 
 	def calculate
+    amount = params[:amount]
+    current_price = currency.current_price
+    value = currency.calculate_value(amount)
+    render json: {
+      currency: currency,
+      current_price_usd: current_price[0],
+      current_price_btc: current_price[1],
+      amount: amount,
+      value_usd: value[0],
+      value_btc: value[1]
+    }
 	end
+
+  private
+
+  def currency
+    @currency ||= Currency.find(params[:id])
+  end
 end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,14 @@
 class Currency < ApplicationRecord
+	def current_price
+    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    request = HTTParty.get(url + self.slug)
+    response = JSON.parse(request.body)[0]
+    price_usd = response['price_usd']
+    price_btc = response['price_btc']
+    return price_usd, price_btc
+	end
+
+  def calculate_value(amount)
+    current_price.map {|value| (value.to_f * amount.to_f).round(4)}
+  end
 end
